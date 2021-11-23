138f808be6a5cdbe18edd65f90d6046839372836
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-23 02:51:15 +0200

    Use swr to show character list

--- modules/characters/components/CharacterList/CharacterList.tsx
+++ modules/characters/components/CharacterList/CharacterList.tsx
@@ -1,7 +1,25 @@
 import { FC } from 'react';
+// hooks
+import { useCharacterList } from '../../hooks/useCharacterList';
 
 const CharacterList: FC = () => {
-  return <div>Character List</div>;
+  const { data, error } = useCharacterList();
+
+  return (
+    <div>
+      <h2>Character List</h2>
+      <div>
+        {data?.results?.map((character) => {
+          return (
+            <div key={character.id}>
+              <div>Name: {character.name}</div>
+              <img src={character.image} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CharacterList;
--- modules/characters/hooks/useCharacterList.tsx
+++ modules/characters/hooks/useCharacterList.tsx
@@ -1,13 +1,22 @@
 import useSWR from 'swr';
+// interfaces
+import { ApiListResponse } from '../../core/core.interface';
+import { Character } from '../character.interface';
 
-const fetcher = () =>
-  fetch('https://rickandmortyapi.com/api/character').then((res) => res.json());
+const fetcher = (api: string) => {
+  return fetch(`https://rickandmortyapi.com/api/${api}`).then((res) => {
+    return res.json();
+  });
+};
 
-export const useCharacterList = () => {
-  const { data, error } = useSWR('characterList', fetcher);
+export const useCharacterList = (page: number = 1) => {
+  const data = useSWR<ApiListResponse<Character[]>, any>(
+    `character?page=${page}`,
+    fetcher
+  );
 
   return {
-    data,
-    error,
+    data: data.data,
+    error: data.error,
   };
 };
